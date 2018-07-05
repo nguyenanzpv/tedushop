c9c5500ab284391cd64712dc3c1ccce4ab876ded
ANNGUYEN\Admin <nguyenanzpv@gmail.com>
2018-07-05 21:03:14 +0700

    create model

---
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        //khoa lien ket voi bang Menu. 1 Menu group co nhieu menu
+        public virtual IEnumerable<Menu> Menus { set; get; }
+    }
+}
